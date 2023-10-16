9a584782c18f51570899cf2058bbdc5963c3211f
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-16 10:01:47 -0400

    update file

--- REST_ APIs_with _Flask _and _Python/03_exercise.py
+++ REST_ APIs_with _Flask _and _Python/03_exercise.py
@@ -1,9 +1,11 @@
+# Get user's name and print it
 name = input("Enter your name: ")
 print(name)
 
-# -- Mathematics on user input --
+# -- Perform math on user's input --
 
+# Get the size of the house in square feet from the user
 size_input = input("How big is your house (in square feet): ")
-square_feet = int(size_input)
-square_metres = square_feet / 10.8  # Make sure this is correct
-print(f"{square_feet} square feet is {square_metres} square metres.")
+square_feet = int(size_input)  # Convert user input to an integer
+square_metres = square_feet / 10.8  # Calculate square meters (approximate conversion)
+print(f"{square_feet} square feet is approximately {square_metres:.2f} square meters.")
