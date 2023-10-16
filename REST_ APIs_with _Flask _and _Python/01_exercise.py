1b0e613f5be6bb5b4767931f95184d80b5997424
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-16 08:43:17 -0400

    add files

---
+++ REST_ APIs_with _Flask _and _Python/01_exercise.py
@@ -0,0 +1,20 @@
+# Numbers and Floats
+x = 15
+price = 9.99
+discount = 0.2
+result = price * (1 - discount)
+print(result)
+
+# Strings
+name = "Rolf"
+print(name)
+print(name * 2)
+
+# Changing variables
+a = b = 25
+print(a)
+print(b)
+
+b = 17
+print(a)
+print(b)
