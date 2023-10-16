d63f43a9c971746ebea88370372c267b8b2f631e
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-16 14:06:30 -0400

    add exercise

--- REST_ APIs_with _Flask _and _Python/08_exercise.py
+++ REST_ APIs_with _Flask _and _Python/08_exercise.py
@@ -1,5 +1,7 @@
+# Prompt the user for the current day of the week.
 day_of_week = input("What day of the week is it today? ")
 
+# Check the day of the week and provide a message accordingly.
 if day_of_week == "Monday":
     print("Have a great start to your week!")
 elif day_of_week == "Friday":
@@ -7,13 +9,13 @@ elif day_of_week == "Friday":
 else:
     print("Full speed ahead!")
 
-# -- Problem: user not entering what we expect --
-
+# Prompt the user for the current day of the week, and convert the input to lowercase.
 day_of_week = input("What day of the week is it today? ").lower()
 
+# Check the day of the week and provide a message accordingly.
 if day_of_week == "monday":
     print("Have a great start to your week!")
 elif day_of_week == "friday":
-    print("It's ok to finish a bit early!")
+    print("It's okay to finish a bit early!")
 else:
     print("Full speed ahead!")
