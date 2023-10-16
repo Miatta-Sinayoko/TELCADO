e3262bba0fd808cb979ac93ee12eab3175f6dc2d
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-16 14:07:08 -0400

    add exercise

--- REST_ APIs_with _Flask _and _Python/09_exercise.py
+++ REST_ APIs_with _Flask _and _Python/09_exercise.py
@@ -1,11 +1,18 @@
+# Create a list of friends.
 friends = ["Rolf", "Bob", "Jen"]
+
+# Check if "Jen" is in the list of friends and print the result.
 print("Jen" in friends)
 
 # --
 
-movies_watched = {"The Matrix", "Green Book", "Her"}
+# Create a set of movies watched.
+movies_watched = {"The Matrix", "Power Book IV", "Renfield"}
+
+# Prompt the user to enter a movie they've watched recently.
 user_movie = input("Enter something you've watched recently: ")
 
+# Check if the user's input is in the set of movies watched and print the result.
 print(user_movie in movies_watched)
 
-# The `in` keyword works in most sequences like lists, tuples, and sets.
+# The `in` keyword is used to check if an element is present in sequences like lists, tuples, and sets.
