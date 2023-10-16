934815e46b500faaaf1d634215c64cab18f9ef80
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-16 12:28:02 -0400

    add file

--- REST_ APIs_with _Flask _and _Python/07_exercise.py
+++ REST_ APIs_with _Flask _and _Python/07_exercise.py
@@ -1,16 +1,27 @@
 # -- Comparisons --
 
-print(5 == 5)  # True
-print(5 > 5)  # False
-print(10 != 10)  # False
+# Check if 5 is equal to 5
+print(5 == 5)  # This is true
 
-# Comparisons: ==, !=, >, <, >=, <=
+# Check if 5 is greater than 5
+print(5 > 5)  # This is false
+
+# Check if 10 is not equal to 10
+print(10 != 10)  # This is false
+
+# -- Comparison Operators: ==, !=, >, <, >=, <= --
 
 # -- is --
-# Python also has the `is` keyword. It's a confusing keyword for now, so I don't recommend using it.
+# Python also has the `is` keyword, but it can be confusing, so it's best to avoid it for now.
 
+# Create two lists of friends
 friends = ["Rolf", "Bob"]
 abroad = ["Rolf", "Bob"]
 
-print(friends == abroad)  # True
-print(friends is abroad)  # False
+# Check if the lists have the same content
+print(friends == abroad)  # This is true
+
+# Check if the lists are the exact same objects in memory (usually not recommended)
+print(friends is abroad)  # This is false
+
+
