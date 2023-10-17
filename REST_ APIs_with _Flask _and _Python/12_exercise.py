519a1e8a8479275abd12b2208be125e7f4849255
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-10-17 08:24:45 -0400

    add exercise

--- REST_ APIs_with _Flask _and _Python/12_exercise.py
+++ REST_ APIs_with _Flask _and _Python/12_exercise.py
@@ -1,27 +1,19 @@
-# Creating a list of squares for numbers 1, 3, and 5
-numbers = [1, 3, 5]
-squares = [x * 2 for x in numbers]
+# Create a list of squares for the numbers 1, 3, and 5 using list comprehension
+squares = [x * x for x in [1, 3, 5]]
 
-# Dealing with strings - Filtering friends whose names start with "S"
+# Filter the friends list to only include friends whose names start with "S" using list comprehension
 friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
-
-# Using a for loop to filter names that start with "S"
-starts_s = []
-for friend in friends:
-    if friend.startswith("S"):
-        starts_s.append(friend)
-print(starts_s)
-
-# Using list comprehension to achieve the same result
 starts_s = [friend for friend in friends if friend.startswith("S")]
+
+# Print the filtered list
 print(starts_s)
 
-# Demonstrating that list comprehension creates a new list
+# Demonstrate that list comprehension creates a new list
 friends = ["Sam", "Samantha", "Saurabh"]
 starts_s = [friend for friend in friends if friend.startswith("S")]
 
-# Printing the original list, the filtered list, and checking if they are the same object
+# Print the original and filtered lists and their object IDs to show that they are different objects
 print(friends)
 print(starts_s)
 print(friends is starts_s)
-print("friends: ", id(friends), " starts_s: ", id(starts_s))
+print(f"friends: {id(friends)} starts_s: {id(starts_s)}")
